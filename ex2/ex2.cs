5af3f4b9bd1d1353e63dec64a36df0374e98f493
Oaska01 <osmanaskar195@gmail.com>
2025-04-25 10:50:45 +0300

    adding ex2 file

---
+++ ex2/ex2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, C# in Cursor!");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+} 

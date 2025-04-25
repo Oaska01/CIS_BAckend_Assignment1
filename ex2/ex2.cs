a25da87d3e0f18084e822f928f4c41ae2ea93c73
Oaska01 <osmanaskar195@gmail.com>
2025-04-25 15:57:47 +0300

    ex3

--- ex2/ex2.cs
+++ ex2/ex2.cs
@@ -2,10 +2,27 @@ using System;
 
 class Program
 {
+    static void maximumOccurence(string word){
+        char[] split = new char[word.Length];
+        bool repeate = false;
+
+        for (int i = 0; i <= word.Length; i++){
+            for (int j = 0; j <= word.Length; j++){
+                if (split[i] == split[j]){
+                    Console.WriteLine(split[i]);
+                    repeate = true;
+                }
+            }
+        }
+        
+        
+
+        // Console.WriteLine(split);
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, C# in Cursor!");
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
+        Console.Write("Enter a word: ");
+        string word = Console.ReadLine();
+        maximumOccurence(word);
     }
 } 
